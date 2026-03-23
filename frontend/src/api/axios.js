d909b53bc51ce0a84988602d74e0d4f88d2c3de3
swathi-nagaraj <swathinagaraj2023@gmail.com>
2026-03-23 12:23:34 +0530

    fix: cors config corrected

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  //baseURL: 'http://localhost:5000/api',
-  baseURL: 'https://code-review-assistant-v692.onrender.com/api',
+  baseURL: 'http://localhost:5000/api',
+  //baseURL: 'https://code-review-assistant-v692.onrender.com/api',
 
 });
 
